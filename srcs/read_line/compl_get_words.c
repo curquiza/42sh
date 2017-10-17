45ad230b4695bb98307654c98cb039875bd54c57
Sarah FRANC <sfranc@e1r1p12.42.fr>
2017-10-17 15:43:29 +0200

    Upd : cmd_completion wip

--- srcs/read_line/compl_get_words.c
+++ srcs/read_line/compl_get_words.c
@@ -6,7 +6,7 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 14:44:22 by curquiza          #+#    #+#             */
-/*   Updated: 2017/10/17 14:47:33 by sfranc           ###   ########.fr       */
+/*   Updated: 2017/10/17 15:42:23 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,34 +71,107 @@ int		ft_find_words_in(DIR *open, t_comp_ctrl *ctrl)
 	return (0);
 }
 
-int		ft_get_bin_words(t_comp_ctrl *ctrl)
+int		ft_check_user_right(t_comp_ctrl *ctrl, char *name)
 {
-	char	**pathtab;
-	DIR		*open;
-	int		i;
-	int		len;
+	struct stat		temp_stat;
+	char			*temp;
 
-	if (ft_strcmp(ctrl->to_find, ".") == 0)
-		return (0);
-	if (!(pathtab = ft_get_pathtab(g_shell)))
-		return (0);
-	len = ft_tablen(pathtab);
-	i = -1;
-	while (++i < len)
+	temp = ft_strjoin(ctrl->path, name);
+	if ((stat(temp, &temp_stat)) != -1\
+			&& ((temp_stat.st_mode & S_IXUSR) == S_IXUSR))
 	{
-		if (!(open = opendir(pathtab[i])))
-			continue;
-		if (ft_find_words_in(open, ctrl) == -1)
-		{
-			ft_tabdel(&pathtab);
-			closedir(open);
-			return (-1);
-		}
-		closedir(open);
+		free(temp);
+		return (1);
 	}
-	ft_tabdel(&pathtab);
+	free(temp);
 	return (0);
 }
+
+int		ft_check_is_dir(t_comp_ctrl *ctrl, char *name)
+{
+	struct stat		temp_stat;
+	char			*temp;
+
+	temp = ft_strjoin(ctrl->path, name);
+	if ((stat(temp, &temp_stat)) != -1 && S_ISDIR(temp_stat.st_mode))
+	{
+		free(temp);
+		return (1);
+	}
+	free(temp);
+	return (0);
+}
+
+int		ft_find_exec_in(DIR *open, t_comp_ctrl *ctrl, int mode_dir)
+{
+	struct dirent	*dir;
+	int				len;
+
+	(void)mode_dir;
+	len = ft_strlen(ctrl->clues);
+	while ((dir = readdir(open)) != NULL)
+	{
+		if (ctrl->len >= COMP_SIZE)
+			return (-1);
+		if ((dir->d_name[0] != '.' || ctrl->clues[0] == '.')
+			&& ft_strncmp(dir->d_name, ctrl->clues, len) == 0)
+		{
+			if (ft_check_user_right(ctrl, dir->d_name) == 1)
+			{
+				ft_complst_pushback(ctrl, ft_complete_name(dir->d_name, ctrl));
+				ctrl->len++;
+			}
+		}
+	}
+	return (0);
+}
+
+//int		ft_get_bin_words(t_comp_ctrl *ctrl)
+//{
+//	char	**pathtab;
+//	DIR		*open;
+//	int		i;
+//	int		len;
+//
+////	if (ft_strcmp(ctrl->to_find, ".") == 0)
+////		return (0);
+//	if (!(pathtab = ft_get_pathtab(g_shell)))
+//		return (0);
+//	len = ft_tablen(pathtab);
+//	i = -1;
+//	while (++i < len)
+//	{
+//		if (!(open = opendir(pathtab[i])))
+//			continue;
+//		if (ft_find_words_in(open, ctrl) == -1)
+//		{
+//			ft_tabdel(&pathtab);
+//			closedir(open);
+//			return (-1);
+//		}
+//		closedir(open);
+//	}
+//	ft_tabdel(&pathtab);
+//	return (0);
+//}
+
+int		ft_get_cmd_words(t_comp_ctrl *ctrl)
+{
+	DIR		*open;
+	int		ret;
+
+	ret = 0;
+	if (ft_strchr(ctrl->word_to_comp, '/'))
+	{
+		if (!(open = opendir(ctrl->path)))
+			return (ret);
+		if (ft_find_exec_in(open, ctrl, 1) == -1)
+			ret = -1;
+		closedir(open);
+	}
+	return (ret);
+}
+
 
 int		ft_get_file_words(t_comp_ctrl *ctrl)
 {
@@ -121,8 +194,7 @@ int		ft_get_all_candidates(t_comp_ctrl *ctrl, t_tc *tool)
 
 	ret = 0;
 	if (ctrl->status == 1)
-		;
-	//	ret = ft_get_bin_words(ctrl);
+		ret = ft_get_cmd_words(ctrl);
 	else if (ctrl->status == 2)
 		ret = ft_get_file_words(ctrl);
 	if (ret == -1)
