474c0338e3d9152236336471d67ea996cee062f1
Sarah FRANC <sfranc@e1r1p12.42.fr>
2017-10-17 12:40:39 +0200

    Norm : parsing completion

--- srcs/read_line/compl_init_reset.c
+++ srcs/read_line/compl_init_reset.c
@@ -6,7 +6,7 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/27 14:48:11 by curquiza          #+#    #+#             */
-/*   Updated: 2017/10/17 12:35:38 by sfranc           ###   ########.fr       */
+/*   Updated: 2017/10/17 12:40:24 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ void	ft_get_comp_word(t_tc *tool, t_comp_ctrl *ctrl)
 	if ((temp = ft_strrchr(ctrl->word_to_comp, '/')))
 	{
 		ctrl->clues = ft_strdup(temp + 1);
-		ctrl->path = ft_strsub(ctrl->word_to_comp, 0, ft_strlen(ctrl->word_to_comp) - ft_strlen(ctrl->clues));
+		ctrl->path = ft_strsub(ctrl->word_to_comp, 0,\
+				ft_strlen(ctrl->word_to_comp) - ft_strlen(ctrl->clues));
 	}
 	else
 		ctrl->clues = ft_strdup(ctrl->word_to_comp);
 }
-
 
 int		ft_get_comp_status(t_tc *tool, t_comp_ctrl *ctrl)
 {
@@ -44,21 +44,12 @@ int		ft_get_comp_status(t_tc *tool, t_comp_ctrl *ctrl)
 	int i;
 
 	len = (ctrl->word_to_comp ? ft_strlen(ctrl->word_to_comp) : 0);
-//	ft_putnbr2("len = ", len);
 	pos = tool->curs_y * (tool->x_max + 1) + tool->curs_x - tool->prompt_len;
-//	ft_putstr("\ntool->buff[pos] = ");
-//	ft_putchar('"');
-//	ft_putchar(tool->buff[pos]);
-//	ft_putchar('"');
 	i = pos - len;
 	if (i != 0)
 		--i;
 	else
 		return (1);
-//	ft_putstr("\ndepart = ");
-//	ft_putchar('"');
-//	ft_putchar(tool->buff[i]);
-//	ft_putchar('"');
 	while (i > 0)
 	{
 		if (tool->buff[i] == ';' || tool->buff[i] == '|'\
@@ -69,32 +60,8 @@ int		ft_get_comp_status(t_tc *tool, t_comp_ctrl *ctrl)
 		else
 			--i;
 	}
-
 	return (0);
 }
-
-//int		ft_get_comp_status(t_tc *tool, int status)
-//{
-//	int		i;
-//	int		ret;
-//
-//	ret = status;
-//	if (status == 0)
-//		ret = 1;
-//	i = tool->curs_y * (tool->x_max + 1) + tool->curs_x - tool->prompt_len;
-//	while (i >= 0 && ft_is_separator(tool->buff[i]) != 1)
-//		i--;
-//	i++;
-//	if (i != 0)
-//	{
-//		i--;
-//		while (i >= 0 && ft_is_separator(tool->buff[i]) == 1)
-//			i--;
-//		if (i != -1)
-//			ret = 2;
-//	}
-//	return (ret);
-//}
 
 void	ft_erase_comp(t_comp_ctrl *ctrl)
 {
@@ -116,7 +83,6 @@ void	ft_erase_comp(t_comp_ctrl *ctrl)
 void	ft_reset_compl(t_comp_ctrl *ctrl)
 {
 	ft_erase_comp(g_shell->comp_ctrl);
-	//ft_strdel(&ctrl->to_find);
 	ft_strdel(&ctrl->word_to_comp);
 	ft_strdel(&ctrl->path);
 	ft_strdel(&ctrl->clues);
