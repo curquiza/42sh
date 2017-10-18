c9a4b77524ab821c70ed3ded1f11a812814d578f
Sarah FRANC <sfranc@e1r1p9.42.fr>
2017-10-18 11:33:10 +0200

    Udd : reset of completion at the end of completion

--- srcs/read_line/completion.c
+++ srcs/read_line/completion.c
@@ -6,7 +6,7 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/25 18:28:19 by curquiza          #+#    #+#             */
-/*   Updated: 2017/10/18 10:56:10 by curquiza         ###   ########.fr       */
+/*   Updated: 2017/10/18 11:32:49 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,5 @@ void	ft_completion(t_tc *tool, t_comp_ctrl *ctrl)
 		}
 		ft_strdel(&match);
 	}
+	ft_reset_compl(g_shell->comp_ctrl);
 }
