51198e65ce4e813da17e38e75f68afdeb1f7e4fa
curquiza <clementine.urquizar@gmail.com>
2017-10-19 14:03:00 +0200

    Upd : safe restore of std_fd

--- srcs/execution/execution.c
+++ srcs/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: curquiza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/09 10:02:07 by curquiza          #+#    #+#             */
-/*   Updated: 2017/10/18 17:48:30 by curquiza         ###   ########.fr       */
+/*   Updated: 2017/10/19 14:02:52 by curquiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,10 @@ void	ft_restore_std_fd(t_ast *ast)
 	close(ast->shell->std_fd[0]);
 	close(ast->shell->std_fd[1]);
 	close(ast->shell->std_fd[2]);
-	ft_bzero(ast->shell->std_fd, sizeof(*ast->shell->std_fd) * 3);
+	//ft_bzero(ast->shell->std_fd, sizeof(*ast->shell->std_fd) * 3);
+	ast->shell->std_fd[0] = 0;
+	ast->shell->std_fd[1] = 0;
+	ast->shell->std_fd[2] = 0;
 }
 
 void	ft_pre_execution(t_ast *ast)
