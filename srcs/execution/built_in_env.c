43eea25d7da7d9a77de84c7565fe5ee015918316
Clurquiz <clementine.urquizar@gmail.com>
2017-10-22 01:55:05 +0200

    Upd : env -i ls fixed

--- srcs/execution/built_in_env.c
+++ srcs/execution/built_in_env.c
@@ -36,14 +36,47 @@ char		**ft_get_new_env(char **arg, char *flags, t_shell *shell)
 	return (new_env);
 }
 
+static char	*ft_get_new_path(void)
+{
+	int		fd;
+	char	*tmp;
+	char	*line;
+	char	*path;
+
+	fd = open("/etc/paths", O_RDONLY);
+	path = NULL;
+	line = NULL;
+	tmp = NULL;
+	while (get_next_line(fd, &line) == 1)
+	{
+		tmp = path;
+		if (tmp)
+			path = ft_strjoin3(tmp, ":", line);
+		else
+			path = ft_strdup(line);
+		ft_strdel(&tmp);	
+		ft_strdel(&line);	
+	}
+	return (path);
+}
+
 t_shell		*ft_get_new_shell(char **arg, char *flags, t_shell *old_shell)
 {
 	t_shell	*new_shell;
 	char	**new_env;
+	char	**utility;
+	char	*new_path;
 
 	new_env = ft_get_new_env(arg, flags, old_shell);
 	new_shell = ft_init_shell(0, NULL, new_env, SHELL_NAME);
 	ft_tabdel(&new_env);
+	utility = ft_get_utility(arg);
+	if (utility && utility[0] && ft_exist_in(new_shell->var_env, "PATH") == 0)
+	{
+		new_path = ft_get_new_path();
+		ft_chg_varval_or_add(&new_shell->var_env, "PATH", new_path);
+		ft_strdel(&new_path);
+	}
 	return (new_shell);
 }
 
