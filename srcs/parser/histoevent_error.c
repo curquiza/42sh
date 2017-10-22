7026a76aa75cf1f8e36aa0f78e47531fef6d3dc5
Clurquiz <clementine.urquizar@gmail.com>
2017-10-22 13:23:41 +0200

    Add : ft_find_exclamchar -> checks if there is at least one '!' in the token. Not checked yet

--- srcs/parser/histoevent_error.c
+++ srcs/parser/histoevent_error.c
@@ -1,1 +1,28 @@
 #include "shell.h"
+
+char	*ft_find_exclamchar(char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == 39)
+			i = ft_next_squote(i, s);
+		if (s[i] == '!')
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_check_histoevent_err(t_lexeme *lex)
+{
+	while (lex)
+	{
+		if (ft_find_exclamchar(lex->s) != NULL)
+			;
+		lex = lex->next;
+	}
+	return (NULL);
+}
