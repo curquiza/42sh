43c007def7cc48561b1339d8ad632bed1e10d584
Clurquiz <clementine.urquizar@gmail.com>
2017-10-22 13:35:39 +0200

    Add : start of ft_expand_histoevent

--- srcs/parser/histoevent_error.c
+++ srcs/parser/histoevent_error.c
@@ -16,12 +16,24 @@ char	*ft_find_exclamchar(char *s)
 	return (NULL);
 }
 
+int		ft_expand_histoevent(char **s)
+{
+	char	*tmp;
+	char	*rslt;
+
+	tmp = ft_find_exclamchar(*s);
+	rslt = ft_strsub(*s, 0, tmp - *s);
+	ft_putendl2_fd("rslt = ", rslt, 1);
+	ft_strdel(&tmp);
+	return (0);
+}
+
 char	*ft_check_histoevent_err(t_lexeme *lex)
 {
 	while (lex)
 	{
 		if (ft_find_exclamchar(lex->s) != NULL)
-			;
+			ft_expand_histoevent(&lex->s);
 		lex = lex->next;
 	}
 	return (NULL);
