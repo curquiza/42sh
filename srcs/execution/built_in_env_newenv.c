a65d770e75fde1e6af2fa736d692b0ae4e4a227b
Clurquiz <clementine.urquizar@gmail.com>
2017-10-22 02:16:33 +0200

    Upd : Norm

---
+++ srcs/execution/built_in_env_newenv.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+
+static char	**ft_get_new_env(char **arg, char *flags, t_shell *shell)
+{
+	char	**new_env;
+	int		i;
+
+	if (ft_strchr(flags, 'i'))
+	{
+		if (!(new_env = (char **)malloc(sizeof(**new_env))))
+			ft_exit("malloc error", 1);
+		new_env[0] = NULL;
+	}
+	else
+		new_env = ft_tabdup(shell->var_env);
+	i = 0;
+	while (arg[i])
+	{
+		if (!ft_strchr(arg[i], '='))
+			break ;
+		ft_chg_varval_or_add(&new_env, arg[i], NULL);
+		i++;
+	}
+	return (new_env);
+}
+
+static char	*ft_get_new_path(void)
+{
+	int		fd;
+	char	*tmp;
+	char	*line;
+	char	*path;
+
+	fd = open("/etc/paths", O_RDONLY);
+	path = NULL;
+	line = NULL;
+	tmp = NULL;
+	while (get_next_line(fd, &line) == 1)
+	{
+		tmp = path;
+		if (tmp)
+			path = ft_strjoin3(tmp, ":", line);
+		else
+			path = ft_strdup(line);
+		ft_strdel(&tmp);	
+		ft_strdel(&line);	
+	}
+	return (path);
+}
+
+t_shell		*ft_get_new_shell(char **arg, char *flags, t_shell *old_shell)
+{
+	t_shell	*new_shell;
+	char	**new_env;
+	char	**utility;
+	char	*new_path;
+
+	new_env = ft_get_new_env(arg, flags, old_shell);
+	new_shell = ft_init_shell(0, NULL, new_env, SHELL_NAME);
+	ft_suppr_var(&new_shell->var_env, "PWD");
+	ft_suppr_var(&new_shell->var_env, "SHLVL");
+	ft_tabdel(&new_env);
+	utility = ft_get_utility(arg);
+	if (utility && utility[0] && ft_exist_in(new_shell->var_env, "PATH") == 0
+		&& ft_is_built_in(utility[0]) == NOT_BUILTIN)
+	{
+		new_path = ft_get_new_path();
+		ft_chg_varval_or_add(&new_shell->var_env, "PATH", new_path);
+		ft_strdel(&new_path);
+	}
+	return (new_shell);
+}
