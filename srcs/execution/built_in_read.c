2214c5c6a64f8fc9fceb101a8fd95f3d765f3723
Sarah FRANC <sfranc@e1r3p15.42.fr>
2017-10-27 14:54:57 +0200

    Upd: parsing option -r

--- srcs/execution/built_in_read.c
+++ srcs/execution/built_in_read.c
@@ -1,17 +1,22 @@
 #include "shell.h"
 
-int		ft_builtin_read(t_ast *ast)
+static char	**ft_read_get_fields(char *r)
 {
 	char	*line;
-	char	**var;
 	char	**field;
-	int		i;
 
+	(void)r;
+	field = NULL;
 	if (get_next_line(STDIN_FILENO, &line) == 1)
 	{
 		field = ft_strsplit(line, ' ');
 	}
-	var = ast->argtab + 1;
+	return (field);
+}
+
+static void	ft_read_assign_field_to_var(char **var, char **field)
+{
+	int		i;
 	i = 0;
 	while (var[i])
 	{
@@ -21,5 +26,28 @@ int		ft_builtin_read(t_ast *ast)
 		write(1, "\n", 1);
 		++i;
 	}
+}
+
+int		ft_builtin_read(t_ast *ast)
+{
+	char	*flags;
+	char	**var;
+	int		flag_error;
+	char	**field;
+
+
+	flags = ft_get_flags(ast->argtab + 1);
+	if ((flag_error = ft_check_illegal_flags(flags, "r")) != 0)
+	{
+		ft_put_usage("read", flag_error, "r");
+		ft_strdel(&flags);
+		return (CMD_FAILURE);
+	}
+	var = ft_get_arg(ast->argtab + 1, flag_error);
+
+	field = ft_read_get_fields(flags);
+
+	ft_read_assign_field_to_var(var, field);
+
 	return (CMD_SUCCESS);
 }
