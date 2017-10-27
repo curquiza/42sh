965a4cf6f7d20adeb4c3111fe540761d387d0ace
Sarah FRANC <sfranc@e1r3p15.42.fr>
2017-10-27 14:38:49 +0200

    Upd: basic read with a gnl, no check of backslash nor parsing.

--- srcs/execution/built_in_read.c
+++ srcs/execution/built_in_read.c
@@ -2,7 +2,24 @@
 
 int		ft_builtin_read(t_ast *ast)
 {
-	(void)ast;
-	ft_putendl("builtin read");
+	char	*line;
+	char	**var;
+	char	**field;
+	int		i;
+
+	if (get_next_line(STDIN_FILENO, &line) == 1)
+	{
+		field = ft_strsplit(line, ' ');
+	}
+	var = ast->argtab + 1;
+	i = 0;
+	while (var[i])
+	{
+		var[i] ? ft_putstr(var[i]) : 0;
+		var[i] ? ft_putstr("=") : 0;
+		field[i] ? ft_putstr(field[i]) : 0;
+		write(1, "\n", 1);
+		++i;
+	}
 	return (CMD_SUCCESS);
 }
