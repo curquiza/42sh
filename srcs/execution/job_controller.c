503a450f73fcfd8f94d58d332292dbce63680300
Clurquiz <clementine.urquizar@gmail.com>
2017-10-28 13:28:44 +0200

    Upd : ft_del_job done. Not tested yet. Certainly full of bugs

--- srcs/execution/job_controller.c
+++ srcs/execution/job_controller.c
@@ -11,6 +11,69 @@ static int	ft_job_exists(t_job *job_lst, t_job *current)
 	return (0);
 }
 
+//void	ft_lexlst_remove_if(t_lexeme **begin_list, void *ref, int mode)
+//{
+//	t_lexeme	*current;
+//
+//	if (*begin_list)
+//	{
+//		current = *begin_list;
+//		while (current->next)
+//		{
+//			if (mode == 0 && ft_strcmp(current->next->s, ref) == 0)
+//				ft_lex_suppr_elem(&(current->next));
+//			else if (mode == 1 && current->next->token == *(int *)ref)
+//				ft_lex_suppr_elem(&(current->next));
+//			else
+//				current = current->next;
+//		}
+//		if (*begin_list)
+//		{
+//			if (mode == 0 && ft_strcmp((*begin_list)->s, ref) == 0)
+//				ft_lex_suppr_elem(begin_list);
+//			else if (mode == 1 && (*begin_list)->token == *(int *)ref)
+//				ft_lex_suppr_elem(begin_list);
+//		}
+//	}
+//}
+
+static void	ft_job_suppr_elem(t_job **elem)
+{
+	t_job	*suppr;
+
+	suppr = *elem;
+	*elem = (*elem)->next;
+	ft_strdel(&suppr->cmd_name);
+	suppr->pgid = 0;
+	suppr->next = NULL;
+	free(suppr);
+}
+
+static void	ft_del_job(t_job **lst, t_job **current)
+{
+	t_job 	*prev;
+	t_job	*tmp;
+
+	if (!lst)
+		return ;
+	if (*lst && *lst == *current)
+	{
+		ft_job_suppr_elem(lst);
+		return ;
+	}
+	tmp = *lst;
+	while (tmp)
+	{
+		prev = tmp;
+		if (tmp == *current)
+		{
+			ft_job_suppr_elem(&tmp);
+			return ;
+		}
+		tmp = tmp->next;
+	}
+}
+
 int		ft_wait_for_job(t_job **job)
 {
 	int		ret;
@@ -25,9 +88,14 @@ int		ft_wait_for_job(t_job **job)
 	}
 	else
 	{
-		ft_strdel(&(*job)->cmd_name);
-		free(*job);
-		*job = NULL;
+		if (ft_job_exists(g_shell->job_lst, *job) == 1)
+			ft_del_job(&g_shell->job_lst, job);
+		else
+		{
+			ft_strdel(&(*job)->cmd_name);
+			free(*job);
+			*job = NULL;
+		}
 	}
 	return (ret);
 }
